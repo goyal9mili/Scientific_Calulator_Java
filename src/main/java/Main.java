4b46ee587b9c54e4ff32f850b5f1e4bbe1189c8b
Mili <goyal9mili@gmail.com>
2021-03-14 00:19:33 +0530

    main file changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,67 +6,78 @@ public class Main {
 
         Scanner reader = new Scanner(System.in);
 
-        System.out.print("Enter the number corresponding to the operator \n 1 : square_root \n " +
-                "2 : factorial \n 3: log \n 4: power \n");
-        char operator = reader.next().charAt(0);
+        while(true)
+        {
+            System.out.print("Enter the number corresponding to the operator \n 1 : square_root \n " +
+                    "2 : factorial \n 3: log \n 4: power \n 5: Exit \n");
+            char operator = reader.next().charAt(0);
 
-        double result;
+            double result = -1;
 
-        switch (operator) {
-            case '1':
-                try{
-                    System.out.print("Enter the number to take root of : ");
-                    double first = reader.nextDouble();
-                    result = Calculator.root(first);
-                    break;
-                }
-                catch(NegativeNumberException e){
+            switch (operator) {
+                case '1':
+                    try{
+                        System.out.print("Enter the number to take root of : ");
+                        double first = reader.nextDouble();
+                        result = Calculator.root(first);
+                        System.out.println("The answer to your request is " + result);
+                        break;
+                    }
+                    catch(NegativeNumberException e){
 //                    System.out.println("Cannot Enter a negative number");
-                       System.exit(0);
-                }
+                        break;
+                    }
 
 
 
 
-            case '2':
-                try{
-                    System.out.print("Enter the number to take factorial of : ");
-                    double second = reader.nextDouble();
-                    result = Calculator.fact(second);
+                case '2':
+                    try{
+                        System.out.print("Enter the number to take factorial of : ");
+                        double second = reader.nextDouble();
+                        result = Calculator.fact(second);
+                        System.out.println("The answer to your request is " + result);
+                        break;
+                    }
+                    catch(NegativeNumberException e){
+                        break;
+                    }
+
+
+                case '3':
+                    try{
+                        System.out.print("Enter the number to take log of : ");
+                        double third = reader.nextDouble();
+                        result = Calculator.log(third);
+                        System.out.println("The answer to your request is " + result);
+                        break;
+                    }
+                    catch(NegativeNumberException e)
+                    {
+                        break;
+                    }
+
+
+                case '4':
+                    System.out.print("Enter base number %n: ");
+                    double base = reader.nextDouble();
+                    System.out.print("Enter the power number : ");
+                    double power = reader.nextDouble();
+                    result = Calculator.powerfunc(base, power);
+                    System.out.println("The answer to your request is " + result);
                     break;
-                }
-                catch(NegativeNumberException e){
+
+                case '5':
                     System.exit(0);
-                }
+
+                default:
+
+                    System.out.println("Error! operator is not correct");
+
+            }
 
 
-            case '3':
-                try{
-                    System.out.print("Enter the number to take log of : ");
-                    double third = reader.nextDouble();
-                    result = Calculator.log(third);
-                    break;
-                }
-                catch(NegativeNumberException e)
-                {
-                    System.exit(0);
-                }
-
-
-            case '4':
-                System.out.print("Enter base number %n: ");
-                double base = reader.nextDouble();
-                System.out.print("Enter the power number : ");
-                double power = reader.nextDouble();
-                result = Calculator.powerfunc(base, power);
-                break;
-
-            default:
-
-                System.out.println("Error! operator is not correct");
-                return;
+        }
         }
 
-        System.out.println("The answer to your request is " + result);
-    }
 }
